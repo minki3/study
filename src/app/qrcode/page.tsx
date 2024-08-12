7fc79a31fdf85a9b002664182a6866204c7cda90
minki3 <86992006+minki3@users.noreply.github.com>
2024-08-12 17:12:12 +0900

    clsx 공부

--- src/app/qrcode/page.tsx
+++ src/app/qrcode/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import QRCode from 'qrcode.react'
 import { useSession } from 'next-auth/react'
+import { CustomnButton } from '@/app/components/ui/CustomButton'
 export default function Qrcode() {
   const session = useSession()
   console.log(session)
@@ -8,9 +9,16 @@ export default function Qrcode() {
   //   id: '12321321',
   //   password: '123131231',
   // }
+  const test = true
   return (
     <div>
       <QRCode value="sdfa" />
+      <CustomnButton
+        label="test"
+        size={test ? 'lg' : 'sm'}
+        color="primary"
+        addClassName="text-red-500"
+      />
     </div>
   )
 }
